4fb2814ebda3633e0cdbe18ca9fbd98e89ca5c4a
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-09-24 14:59:10 +0200

    random..

---
+++ src/Rand.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Rand {
+    public static void main(String[] args) {
+
+        int friendsBirthYear = 3118;
+        int myBirthyear = 1991;
+        System.out.println("My friend's birthyear is: " + friendsBirthYear);
+        System.out.println("My birthyear is: " + myBirthyear);
+        System.out.println("The age difference of us is: " + (friendsBirthYear - myBirthyear) + " Years");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hvor gammel er du?");
+        int dinAlder = input.nextInt();
+
+        System.out.println("Hvor gammel er din ven?");
+        int dinVensAlder = input.nextInt();
+
+        System.out.println("Aldersforskellen på jer er " + (dinAlder - dinVensAlder));
+
+        Computer myComputer = new Computer("intel i5", "8gb", "256 gb", "Nvidia geforce something", true);
+        System.out.println("my computers CPU is: " + myComputer.RAM);
+    }
+}
