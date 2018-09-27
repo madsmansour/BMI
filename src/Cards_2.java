abe188048f5443715bdae620b45c2f8d7f9624fb
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-09-27 11:38:06 +0200

    random card

---
+++ src/Cards_2.java
@@ -0,0 +1,166 @@
+public class Cards_2 {
+    public static void main(String[] args) {
+
+        int key = (int) ((Math.random()*((52-1)+1)));
+
+
+        switch (key){
+        case 1:
+        System.out.println("You got the ace of spades");
+        break;
+        case 2:
+        System.out.println("You got the ace of hearts");
+        break;
+        case 3:
+        System.out.println("You got the ace of clubs");
+        break;
+        case 4:
+        System.out.println("You got the ace of diamonds");
+        break;
+        case 5:
+        System.out.println("You got 2 of spades");
+        break;
+        case 6:
+        System.out.println("You got 2 of hearts");
+        break;
+        case 7:
+        System.out.println("You got 2 of clubs");
+        break;
+        case 8:
+        System.out.println("You got 2 of diamonds");
+        break;
+        case 9:
+        System.out.println("You got 3 of spades");
+        break;
+        case 10:
+        System.out.println("You got 3 of hearts");
+        break;
+        case 11:
+        System.out.println("You got 3 of clubs");
+        break;
+        case 12:
+        System.out.println("You got 3 of diamonds");
+        break;
+        case 13:
+        System.out.println("You got 4 of spades");
+        break;
+        case 14:
+        System.out.println("You got 4 of hearts");
+        break;
+        case 15:
+        System.out.println("You got 4 clubs");
+        break;
+        case 16:
+        System.out.println("You got 4 of diamonds");
+        break;
+        case 17:
+        System.out.println("You got 5 of spades");
+        break;
+        case 18:
+        System.out.println("You got 5 of hearts");
+        break;
+        case 19:
+        System.out.println("You got 5 of clubs");
+        break;
+        case 20:
+        System.out.println("You got 5 of diamonds");
+        break;
+        case 21:
+        System.out.println("You got 6 of spades");
+        break;
+        case 22:
+        System.out.println("You got 6 of clubs");
+        break;
+        case 23:
+        System.out.println("You got 6 of diamonds");
+        break;
+        case 24:
+        System.out.println("You got 7 of spades");
+        break;
+        case 25:
+        System.out.println("You got 7 of hearts");
+        break;
+        case 26:
+        System.out.println("You got 7 of clubs");
+        break;
+        case 27:
+        System.out.println("You got 7 of diamonds");
+        break;
+        case 28:
+        System.out.println("You got 8 of spades");
+        break;
+        case 29:
+        System.out.println("You got 8 of hearts");
+        break;
+        case 30:
+        System.out.println("You got the 8 of clubs");
+        break;
+        case 31:
+        System.out.println("You got 8 of diamonds");
+        break;
+        case 32:
+        System.out.println("You got 9 of spades");
+        break;
+        case 33:
+        System.out.println("You got 9 of hearts");
+        break;
+        case 34:
+        System.out.println("You got 9 of clubs");
+        break;
+        case 35:
+        System.out.println("You got 9 of diamonds");
+        break;
+        case 36:
+        System.out.println("You got 10 of spades");
+        break;
+        case 37:
+        System.out.println("You got 10 of hearts");
+        break;
+        case 38:
+        System.out.println("You got 10 of clubs");
+        break;
+        case 39:
+        System.out.println("You got 10 of diamonds");
+        break;
+        case 40:
+        System.out.println("You got the jack of spades");
+        break;
+        case 41:
+        System.out.println("You got the jack of hearts");
+        break;
+        case 42:
+        System.out.println("You got the jack of clubs");
+        break;
+        case 43:
+        System.out.println("You got the jack of diamonds");
+        break;
+        case 44:
+        System.out.println("You got the queen of spades");
+        break;
+        case 45:
+        System.out.println("You got the queen of hearts");
+        break;
+        case 46:
+        System.out.println("You got the queen of clubs");
+        break;
+        case 47:
+        System.out.println("You got the queen of diamonds");
+        break;
+        case 48:
+        System.out.println("You got the king of spades");
+        break;
+        case 49:
+        System.out.println("You got the king of hearts");
+        break;
+        case 50:
+        System.out.println("You got the king of clubs");
+        break;
+        case 51:
+        System.out.println("You got the king of diamonds");
+        break;
+        case 52:
+        System.out.println("You got the 6 of hearts");
+        break;
+
+    }
+}}
